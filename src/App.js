bb5df80cc1a987d2b943f54ed8e923edfa80ea67
Michael Christian Buratti <mburatti@users.noreply.github.com>
2019-08-29 08:29:17 -0400

    Created basic Dashboard, no Backend connection
    Created basic Dashboard, no Backend connection

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
-import './css/App.css';
 import Dashboard from "./components/Dashboard";
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/App.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProject from './components/project/AddProject';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
-        <Dashboard />
-        <Dashboard />
-        <Dashboard />
-      </div>
+      <Router>
+          <div className="App" >
+            <Header/>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+      </Router>
     );
   }
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,26 @@
-import React, {
-    Component
-} from 'react'
+import React, {Component} from 'react';
+import ProjectItem from './project/ProjectItem';
+import CreateProjectButton from './project/CreateProjectButton';
 
 class Dashboard extends Component {
     render() {
         return (
-            <h1> Welcome to Dashboard </h1>
+            <div>
+                <div className="projects">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h1 className="display-4 text-center">Projects</h1>
+                                <br />
+                                    <CreateProjectButton />
+                                <br />
+                                <hr />
+                                <ProjectItem/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
